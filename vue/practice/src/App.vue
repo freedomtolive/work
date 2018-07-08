e86f91b5b9a9f46fae49be93800a739dbac7317c
freedomtolive <liutao402955169@qq.com>
2018-07-08 13:45:53 +0800

    2018/7/8

--- vue/practice/src/App.vue
+++ vue/practice/src/App.vue
@@ -1,23 +1,45 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <div class="nav-box">
+      <ul class="nav">
+          <!-- tag:要生成的标签，默认为a标签 -->
+        <router-link :to=index tag="li">
+          <i class="fa fa-home"></i>
+          <span>index</span>
+        </router-link>
+        <li>
+          <!-- 设置独立的选中时的class -->
+          <router-link :to="{path:'/child'}" active-class="isActive">child</router-link>
+        </li>
+        <li>
+          <router-link to="/about">about</router-link>
+        </li>
+      </ul>
+    </div>
+    <!-- 想在每个替换的组件中加相同的class，可以加在router-view上
+        如果子标签也填加了class，router-vue的class也会加在子元素上，和子元素的标签组合 -->
+    <router-view class="center"/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data(){
+    return {
+      index:'/index'
+    }
+  }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
--- vue/practice/src/router/index.js
+++ vue/practice/src/router/index.js
@@ -2,21 +2,29 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import index from '@/components/index'
 import child from '@/components/child'
+import about from '@/components/about'
 
 // 将路由作为vue的插件
 Vue.use(Router)
 
 export default new Router({
+  mode:"history",
+  linkActiveClass:"on",/* 设置路由选中的class */
   routes: [
     {
-      path: '/',
+      path: '/index',
       name: 'index',
       component: index
     },
     {
-      path:'/childPage',
-      name: 'childPage',
+      path:'/child',
+      name: 'child',
       component: child
+    },
+    {
+      path:'/about',
+      name: 'about',
+      component: about
     }
   ]
 })
