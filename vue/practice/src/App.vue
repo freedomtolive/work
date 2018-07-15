2f54a23e804c3e05de8ee7e780f8ba34bb452fc4
freedomtolive <liutao402955169@qq.com>
2018-07-15 19:48:48 +0800

    vue

--- vue/practice/src/App.vue
+++ vue/practice/src/App.vue
@@ -25,6 +25,19 @@
         </li>
       </ul>
     </div>
+
+    <!-- 
+      编程式导航：
+        利用按钮控制路由实现网页的前进后退
+     -->
+    <input type="button" value="后退" @click="backHandle">
+    <input type="button" value="前进" @click="fowardHandle">
+    <input type="button" value="控制前进后退的步数" @click="goHandle">
+    <input type="button" value="控制指定导航push" @click="pushHandle">
+    <input type="button" value="控制指定导航replace" @click="replacehHandle">
+
+
+
     <router-view name="silderName"/> <!-- 命名视图：渲染多个组件时会使用name -->
     <!-- 想在每个替换的组件中加相同的class，可以加在router-view上
         如果子标签也填加了class，router-vue的class也会加在子元素上，和子元素的标签组合 -->
@@ -38,6 +51,7 @@
     <transition :name="names">
       <router-view class="center"/>
     </transition>
+    
   </div>
 </template>
 
@@ -51,31 +65,40 @@ export default {
       names:'left'
     }
   },
-  // watch:{
-  //   $route(to,from){
-  //     // to:进入的路由信息 from：离开的路由信息
-  //     console.log("当前的路由下标：" + to.meta.index) 
-  //     console.log("之前的路由下标：" + from.meta.index)
+  watch:{
+    $route(to,from){
+      // to:进入的路由信息 from：离开的路由信息
+      // console.log("当前的路由下标：" + to.meta.index) 
+      // console.log("之前的路由下标：" + from.meta.index)
       
-  //     if(to.meta.index < from.meta.index){
-  //       this.names = "right";
-  //     }else{
-  //       this.names = "left";
-  //     }
-  //   }
-  // }
-  watch: {
-    $route(to, from){
-      console.log(to.meta.index)  // 目标导航下标
-      console.log(from.meta.index)  // 离开导航下标
-
-      if(to.meta.index < from.meta.index) {
-        this.names = 'right'
+      if(to.meta.index < from.meta.index){
+        this.names = "right";
       }else{
-        this.names = 'left'
+        this.names = "left";
       }
     }
   },
+  methods:{
+    backHandle(){
+      this.$router.back();
+    },
+    fowardHandle(){
+      this.$router.forward();
+    },
+    goHandle(){
+      // 参数为步数，-1为后退1步，1为前进1步，0为刷新页面
+      // 如果参数超过历史记录，则会无效
+      this.$router.go(-1);
+    },
+    pushHandle(){
+      // this.$router.push('/child#abc');
+      this.$router.push({path:'/child#abc'}); //会添加一个历史记录
+    },
+    replacehHandle(){
+      // this.$router.replace('/child#abc');
+      this.$router.replace({path:'/child#abc'});  //会替换最后一个历史记录
+    }
+  }
 }
 </script>
 
