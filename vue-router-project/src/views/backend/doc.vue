c14bd28f63b4aae7828aaed1f3418220dd0c10c2
freedomtolive <liutao402955169@qq.com>
2018-07-24 19:35:59 +0800

    vue-router-project

--- vue-router-project/src/views/backend/doc.vue
+++ vue-router-project/src/views/backend/doc.vue
@@ -39,8 +39,58 @@
 </template>
 
 <script>
-  
+  import DocView from '@/views/backend/doc-view';
+  // 添加tween.js
+  import TWEEN from 'tween.js';
+
+  export default{
+    components : {
+      DocView
+    },
+    // watch:{
+    //   $route(){
+    //     console.log(this.$route.hash.slice(1))
+    //   }
+    // }
+    beforeRouteUpdate(to,from,next){
+      // console.log(to.hash)
+      this.animated(to);
+      next();
+    },
+    methods:{
+      animated(to){
+
+        
+        // function animateFrame(time){
+        //   requestAnimationFrame(animateFrame);
+        //   TWEEN.update(time);
+        // }
+
+        // if(to.hash){
+        //   var el = document.getElementById(to.hash.slice(1));
+        //   var doc = document.getElementsByClassName("doc")[0];
+
+        //   if(el){
+        //     new TWEEN.Tween({
+        //       number:doc.scrollTop   //起始位置
+        //     })
+        //     .to({
+        //       number:el.offsetTop   //结束位置
+        //     })
+        //     .onUpdate(function(){
+        //       doc.scrollTop = this.number.toFixed(0)
+        //     })
+        //     .start();
+        //   }
+        // }else{
+
+        // }
+      }
+    }
+  }
+
 </script>
+
 <style>
 
 </style>
