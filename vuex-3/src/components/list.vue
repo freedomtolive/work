6dc2452a9dec04462812c430fc006c8c3c6ea5f5
freedomtolive <liutao402955169@qq.com>
2018-09-11 20:25:49 +0800

    vuex-3 select

---
+++ vuex-3/src/components/list.vue
@@ -0,0 +1,17 @@
+<template>
+   <ul class="list">
+        <li >123</li>
+   </ul>
+</template>
+
+<script>
+
+
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
---
+++ vuex-3/src/components/select.vue
@@ -0,0 +1,41 @@
+<template>
+    <section class="warp">
+      <div class="searchIpt clearFix">
+          <select-input>111</select-input>
+          <list></list>
+      </div>
+    </section>
+</template>
+
+<script>
+
+import selectInput from '@/components/selectInput'
+import list from '@/components/list'
+
+let listData = [
+    {
+        title : "javascript"
+    },
+    {
+        title : "html+css"
+    },
+    {
+        title : "jQuery"
+    },
+    {
+        title : "react"
+    },
+    {
+        title : "node.js"
+    }
+]
+
+export default {
+  
+}
+</script>
+
+
+<style>
+
+</style>
